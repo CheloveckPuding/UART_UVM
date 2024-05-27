7fb82ffd2e0725cba11c094b49b6e7296686096b
Ivan Volkov <volkovvana86@gmail.com>
2024-05-28 01:20:12 +0300

    Created uvm_scoreboard

---
+++ sim/uvm_uart_scoreboard.sv
@@ -0,0 +1,33 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+class uvm_uart_scoreboard extends  uvm_uart_scoreboard;
+
+/*-------------------------------------------------------------------------------
+-- Interface, port, fields
+-------------------------------------------------------------------------------*/
+	
+
+/*-------------------------------------------------------------------------------
+-- UVM Factory register
+-------------------------------------------------------------------------------*/
+	// Provide implementations of virtual methods such as get_type_name and create
+	`uvm_component_utils(uvm_uart_scoreboard)
+
+/*-------------------------------------------------------------------------------
+-- Functions
+-------------------------------------------------------------------------------*/
+	// Constructor
+	function new(string name = "uvm_uart_scoreboard", uvm_component parent=null);
+		super.new(name, parent);
+	endfunction : new
+
+	function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+	endfunction : build_phase
+
+	task run_phase(uvm_phase phase);
+		super.run_phase(phase);
+		`uvm_info("Scoreboard","Scoreboard is running", )
+	endtask : run_phase
+
+endclass : uvm_uart_scoreboard
